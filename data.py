0db6145513f1c4dc9359648a01a5f49f2a515f6a
George Tait <george.tait512@gmail.com>
2024-11-16 13:56:27 +0000

    Updated Data Classification
    Edited labelling to work in binary. 1-> Is a gamma particle. 0->is a Hadron.

--- data.py
+++ data.py
@@ -3,6 +3,10 @@ import numpy as np
 # import tensorflow as tf
 import matplotlib.pyplot as plt
 
-col_names = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAssym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+col_names = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAssym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "isGamma"]
 dat = pd.read_csv('MAGIC/magic04.data', names=col_names)
+
+#classifying gamma as 1, hadron as 0. 
+dat['isGamma'] = (dat["isGamma"] == 'g').astype(int)
+
 print(dat)
